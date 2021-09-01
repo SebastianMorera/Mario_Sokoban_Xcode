519da772e4da2c2c9514eba3e3645b283244a84d
Sebastian Morera <sebas.morera999@hotmail.com>
2021-08-31 22:31:51 -0400

    Initial Commit

---
+++ Mario_Sokoban/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Mario_Sokoban
+//
+//  Created by Sebastian Morera on 2021-08-31.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
